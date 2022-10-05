a820cb1f5afdac6aa5bc1ae62d6b1b64cba5cf4e
coverfox <bhavishya2107>
2022-10-06 00:03:55 +0530

    add vuetify, remove unwanted components, add navbar

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <h2 class="font-weight-medium">Interview Management</h2>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+
+  data: () => ({}),
+};
+</script>
